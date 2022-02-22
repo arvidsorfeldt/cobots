c52291aff0d7b197bb840d884344b14ebe562e29
arvidsorfeldt <arvid.sorfeldt@gmail.com>
2022-02-22 14:20:48 +0100

    Install script done

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cobots
+```console
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sudo apt install libclang-dev ros-galactic-xacro
+chmod +x install.bash
+./install.bash
+```
--- install.bash
+++ install.bash
@@ -9,4 +9,11 @@ git clone https://github.com/sequenceplanner/ur_script_driver.git $PATH_TO_SRC/u
 git clone https://github.com/sequenceplanner/ur_script_msgs.git $PATH_TO_SRC/ur_script_msgs
 git clone https://github.com/sequenceplanner/ur_tools.git $PATH_TO_SRC/ur_tools
 git clone https://github.com/sequenceplanner/viz_tools.git $PATH_TO_SRC/viz_tools
-
+git clone https://github.com/sequenceplanner/tf_tools_msgs.git $PATH_TO_SRC/tf_tools_msgs
+git clone https://github.com/sequenceplanner/ur_tools_msgs.git $PATH_TO_SRC/ur_tools_msgs
+cd dep_ws/src/ur_tools
+git reset --hard 850bc0440a740f85bccb25f00d5fbe642f819a6f
+cd ..
+cd ..
+colcon build
+cd ..
