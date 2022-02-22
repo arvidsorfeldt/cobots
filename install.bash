190be0eae8e6d6310c9df0c0cdf4431b2f9ebd24
arvidsorfeldt <arvid.sorfeldt@gmail.com>
2022-02-22 13:31:27 +0100

    Added install script

---
+++ install.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+PATH_TO_SRC="./dep_ws/src/" 
+git clone https://github.com/sequenceplanner/aruco_handler.git $PATH_TO_SRC/aruco_handler
+git clone https://github.com/sequenceplanner/gui_tools.git $PATH_TO_SRC/gui_tools
+git clone https://github.com/sequenceplanner/mp_msgs.git $PATH_TO_SRC/mp_msgs
+git clone https://github.com/sequenceplanner/opcua_ros2_bridge.git $PATH_TO_SRC/opcua_ros2_bridge
+git clone https://github.com/sequenceplanner/tf_tools.git $PATH_TO_SRC/tf_tools
+git clone https://github.com/sequenceplanner/ur_script_driver.git $PATH_TO_SRC/ur_script_driver
+git clone https://github.com/sequenceplanner/ur_script_msgs.git $PATH_TO_SRC/ur_script_msgs
+git clone https://github.com/sequenceplanner/ur_tools.git $PATH_TO_SRC/ur_tools
+git clone https://github.com/sequenceplanner/viz_tools.git $PATH_TO_SRC/viz_tools
+
